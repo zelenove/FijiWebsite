6ede0a638e22997be12b4abc2a9d730bb5a0e307
Eugene Zelenov <eugenezelenov@gmail.com>
2018-03-18 17:59:09 -0400

    barebones layout completed, links fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root :to =>'static_pages#home'
+  root 'static_pages#home'
   get  '/alumni' => 'static_pages#alumni'
   get  '/brothers' => 'static_pages#brothers'
   get  '/chapter'=> 'static_pages#chapter'
-  get '*path' => redirect('/')
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
